6380aa2bff2890868a8be80e6a54528b9fc6cf47
haturatu <warsaw21g@gmail.com>
2024-06-23 03:27:45 +0900

    fix all

--- _config.ts
+++ _config.ts
@@ -1,10 +1,13 @@
 import lume from "lume/mod.ts";
+import blog from "blog/mod.ts";
 import plugins from "./plugins.ts";
+import nunjucks from "lume/plugins/nunjucks.ts";
 
 const site = lume({
   src: "./src",
 });
 
 site.use(plugins());
+site.use(nunjucks());
 
 export default site;

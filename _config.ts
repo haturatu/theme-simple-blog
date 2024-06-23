4be6ea172e222c9fda0dad0f7d910fb6d77e1641
haturatu <warsaw21g@gmail.com>
2024-06-23 14:45:51 +0900

    index pages content trancate

--- _config.ts
+++ _config.ts
@@ -2,8 +2,6 @@ import lume from "lume/mod.ts";
 import blog from "blog/mod.ts";
 import plugins from "./plugins.ts";
 import nunjucks from "lume/plugins/nunjucks.ts";
-import { getCategories } from "./getCategories.ts";
-import matter from "gray-matter";
 
 const site = lume({
   src: "./src",
@@ -11,35 +9,6 @@ const site = lume({
 
 site.use(plugins());
 site.use(nunjucks());
-
-async function generateCategoryPages() {
-  const categorizedPosts = await getCategories();
-
-  for (const category in categorizedPosts) {
-    const posts = categorizedPosts[category];
-    let pageContent = `---
-title: "${category}の投稿"
----
-
-<ul>
-`;
-
-    posts.forEach(post => {
-      pageContent += `<li>${post.title} - ${post.date}</li>\n`;
-    });
-
-    pageContent += `</ul>\n`;
-
-    const filePath = `./src/posts/${category}.md`;
-
-    // ファイルに書き込む
-    await Deno.writeTextFile(filePath, pageContent);
-
-    console.log(`Generated ${filePath}`);
-  }
-}
-
-await generateCategoryPages();
 
 export default site;
 
